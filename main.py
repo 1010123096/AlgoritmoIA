b460ee3cd98da2a09f543b461017c67e112c8091
1010123096 <saariza@unicesar.edu.co>
2023-05-06 12:04:43 -0500

    cambios

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from readXlsx.readData import readData
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw() # Oculta la ventana principal
+
+# Abre una ventana de diálogo para buscar el archivo
+file_path = filedialog.askopenfilename(title="Seleccionar archivo", filetypes=[("Excel files", "*.xlsx")])
+
+# Verifica si se seleccionó un archivo
+if file_path:
+    # Realiza las operaciones necesarias con el archivo
+    print("Se seleccionó el archivo:", file_path)
+else:
+    print("No se seleccionó ningún archivo")
 
 
-
-ruta = "readXlsx\pruebasubirArchivos.xlsx"
+ruta = file_path
 
 print(readData(ruta))
 
