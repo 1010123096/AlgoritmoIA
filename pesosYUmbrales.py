2a817a5cd13797eb24bed25d4ce21756a2a99ad3
1010123096 <saariza@unicesar.edu.co>
2023-05-08 16:46:56 -0500

    creacionde funciones para guardar pesos y umbrales

--- pesosYUmbrales.py
+++ pesosYUmbrales.py
@@ -1,28 +1,43 @@
 import numpy as np
+import openpyxl
 
+def guardarPesos(matriz,nombre):
+  workbook = openpyxl.Workbook()
+  sheet = workbook.active
+  for row in matriz:
+    sheet.append(row)
+  workbook.save("archivosExcelGuardados/"+nombre+'.xlsx')
+  
+def guardarUmbrales(x,nombre):
+  workbook = openpyxl.Workbook()
+  sheet = workbook.active 
+  sheet.append(x)
+  workbook.save("archivosExcelGuardados/"+nombre+'.xlsx')
+  
 
-
-
-def matrizPesos(entradas,salidas):
-    matrizPesos = np.random.uniform(low=-1, high=1, size=(entradas,salidas))
-    matrizPesos = np.round(matrizPesos,decimals=2)
+def matrizPesos(entradas, salidas):
+    matrizPesos = np.random.uniform(low=-1, high=1, size=(entradas, salidas))
+    matrizPesos = np.round(matrizPesos, decimals=2)
     return matrizPesos
+
 
 def matrizUmbrales(salidas):
     matrizUmbrales = np.random.uniform(low=-1, high=1, size=(salidas))
-    matrizUmbrales = np.round(matrizUmbrales,decimals=2)
+    matrizUmbrales = np.round(matrizUmbrales, decimals=2)
     return matrizUmbrales
-
 
 
 print("Matriz De peso")
 
-entradas= int(input("Entradas: \n"))
-salidas= int(input("Salidas: \n"))
-
-print(matrizPesos(entradas,salidas))
+entradas = int(input("Entradas: \n"))
+salidas = int(input("Salidas: \n"))
+mp=matrizPesos(entradas, salidas)
+matrizp=mp.tolist()
+print(matrizp)
 print("Matriz De umbrales")
-
-print(matrizUmbrales(salidas))
-
+mu =matrizUmbrales(salidas)
+matrizu=mu.tolist()
+print(matrizu)
+guardarPesos(matrizp,"pesos")
+guardarUmbrales(matrizu,"Umbrales")
 
--- readXlsx/readData.py
+++ readXlsx/readData.py
@@ -2,56 +2,43 @@ import openpyxl
 from tabulate import tabulate
 
 
-
 def readData(ruta):
 
-     # Define variable to load the dataframe
-     excel_dataframe = openpyxl.load_workbook(ruta)
+    # Define variable to load the dataframe
+    excel_dataframe = openpyxl.load_workbook(ruta)
 
-     # Define variable to read sheet
-     dataframe = excel_dataframe.active
+    # Define variable to read sheet
+    dataframe = excel_dataframe.active
 
-     print(dataframe)
+    print(dataframe)
 
-     encabezado=[]
-     data = []
+    encabezado = []
+    data = []
 
+    # se definen las variables de entradas y salidas de la matriz
+    entradas = 0
+    salidas = 0
+    valoresNulos = 0
+    # recorre las columnas para contar entradas y salidas
+    for col in dataframe.iter_cols(0, dataframe.max_column):
 
+        if "X" in col[0].value:
+            entradas += 1
+        else:
+            if "S" in col[0].value:
+                salidas += 1
+            else:
+                valoresNulos += 1
+    # variable de la cantidad de patrones
+    patrones = 0
+    # recorre las filas para contar los patrones
+    for row in range(1, dataframe.max_row):
+        patrones += 1
 
+    return entradas, salidas, patrones
 
-     # se definen las variables de entradas y salidas de la matriz
-     entradas=0;
-     salidas=0;
-     valoresNulos=0;
-     # recorre las columnas para contar entradas y salidas
-     for col in dataframe.iter_cols(0, dataframe.max_column):
-          
-          
-          if "X" in col[0].value:
-               entradas+=1;
-          else:
-               if "S" in col[0].value:
-                    salidas+=1;
-               else:
-                    valoresNulos+=1;
-     # variable de la cantidad de patrones
-     patrones=0;
-     # recorre las filas para contar los patrones
-     for row in range(1, dataframe.max_row):
-          patrones+=1;
+    # data.append(_row)
 
-     return entradas,salidas,patrones
-     
-                    
-          
+    # print(tabulate(data))
 
-     #data.append(_row)
-
-     #print(tabulate(data))
-
-     #print(tabulate(data,headers=encabezado))
-     
-     
-     
-
-     
+    # print(tabulate(data,headers=encabezado))
