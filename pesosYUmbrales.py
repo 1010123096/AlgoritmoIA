6c6ec6c86cf073b64490f968169c0cecfa96c0b9
1010123096 <saariza@unicesar.edu.co>
2023-05-09 11:16:15 -0500

    creación de funcion para leer los archivos de pesos y humbrales se llama readDataExcel

--- pesosYUmbrales.py
+++ pesosYUmbrales.py
@@ -7,15 +7,18 @@ def guardarPesos(matriz,nombre):
   sheet = workbook.active
   for row in matriz:
     sheet.append(row)
-  workbook.save("archivosExcelGuardados/"+nombre+'.xlsx')
+    rutaPesos="archivosExcelGuardados/"+nombre+'.xlsx'
+    workbook.save(rutaPesos)
+  return rutaPesos
   
 def guardarUmbrales(matriz,nombre):
   matriz=matriz.tolist()
   workbook = openpyxl.Workbook()
   sheet = workbook.active 
   sheet.append(matriz)
-  workbook.save("archivosExcelGuardados/"+nombre+'.xlsx')
-  
+  rutaUmbrales="archivosExcelGuardados/"+nombre+'.xlsx'
+  workbook.save(rutaUmbrales)
+  return rutaUmbrales
 
 def matrizPesos(entradas, salidas):
     matrizPesos = np.random.uniform(low=-1, high=1, size=(entradas, salidas))
@@ -29,6 +32,20 @@ def matrizUmbrales(salidas):
     return matrizUmbrales
 
 
+def readDataExcel(ruta):    
+    # Define variable to load the dataframe
+    excel_dataframe = openpyxl.load_workbook(ruta)
+    # Define variable to read sheet
+    dataframe = excel_dataframe.active
+    print(dataframe)
+    data = []
+# Recorre el archibo de umbrales y los guarda en data y los imprime
+    for col in dataframe.iter_cols(0, dataframe.max_column):
+      for row in range(0, dataframe.max_row):       
+        data.append(col[row].value)
+    
+    return data
+
 print("Matriz De peso")
 
 entradas = int(input("Entradas: \n"))
@@ -38,6 +55,10 @@ print(matrizp)
 print("Matriz De umbrales")
 matrizu =matrizUmbrales(salidas)
 print(matrizu)
-guardarPesos(matrizp,"pesos")
-guardarUmbrales(matrizu,"Umbrales")
+rutaPesos= guardarPesos(matrizp,"pesos")
+rutaUnmbrales= guardarUmbrales(matrizu,"Umbrales")
+print(rutaPesos)
+lecturaMatrizP=readDataExcel(rutaPesos)
+print(lecturaMatrizP)
+
 
